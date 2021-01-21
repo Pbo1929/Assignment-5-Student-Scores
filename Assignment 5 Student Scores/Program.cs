d73fbb2c8b25d5938258e6b1e19163cc76843eea
Prem Worapon Sumsai <lulztit1929@gmail.com>
2021-01-21 14:53:13 +0700

    Add project files.

---
+++ Assignment 5 Student Scores/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Assignment_5_Student_Scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] toysScores = { 7, 6, 5, 4, 3 };
+            Student Toy = new Student("Toy", 12, toysScores);
+            Console.WriteLine(Toy.GetAverage());
+            Console.WriteLine(Toy.GetHighest());
+            Console.WriteLine(Toy.GetLowest());
+
+            Student intouch = new Student("Intouch", 12, 7, 7, 3, 2, 5);
+            Console.WriteLine(intouch.GetAverage());
+            Console.WriteLine(intouch.GetHighest());
+            Console.WriteLine(intouch.GetLowest());
+            int[] intouchScores = intouch.GetScores();
+            Console.WriteLine(intouchScores[0]);
+            for(int i = 0; i < intouchScores.Length; i++)
+            {
+                Console.WriteLine(intouchScores[i]);
+            }
+        }
+    }
+}
---
+++ Assignment 5 Student Scores/Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_5_Student_Scores
+{
+    class Student
+    {
+        private string name;
+        private int year;
+        //Gives 5 spaces to keep scores.
+        private int[] scores;
+
+        public Student(string name, int year, int s1, int s2, int s3,
+            int s4, int s5)
+        {
+            this.name = name;
+            this.year = year;
+            scores[0] = s1;
+            scores[1] = s2;
+            scores[2] = s3;
+            scores[3] = s4;
+            scores[4] = s5;
+        }
+
+        public Student(string name, int year, int[] scores)
+        {
+            this.name = name;
+            this.year = year;
+            this.scores = scores;
+        }
+
+        public double GetAverage()
+        {
+            int total = 0;
+            for(int i = 0; i < scores.Length; i++)
+            {
+                total = total + scores[i];
+            }
+            double average = (double)total / scores.Length;
+                return average;
+        }
+
+        public int GetHighest()
+        {
+            int highest = scores[0];
+            for(int i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] > highest) highest = scores[i];
+            }
+            return highest;
+        }
+
+        public int GetLowest()
+        {
+            int lowest = scores[0];
+            for(int i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] < lowest) lowest = scores[i];
+            }
+            return lowest;
+        }
+
+        public int[] GetScores()
+        {
+            return scores;
+        }
+    }
+}
